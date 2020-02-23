acd16a82b330b704e27a8d45501b0b19e7f46a42
danielgrindstaff <danielgrindstaff@192.168.254.137>
2020-02-23 18:55:14 +0800

    Some cosmetic changes made to header and footer

--- lifetrack/includes/header.php
+++ lifetrack/includes/header.php
@@ -1,7 +1,7 @@
 <header>
     <div class="row">
         <div class="col-lg-12" align="center">
-            <h1>Lifetrack Medical Systems Growth Forecast - 2020</h1>
+            <h1>Lifetrack Medical Systems Growth Forecast - 2020 </h1>
         </div>
     </div>
 </header>
